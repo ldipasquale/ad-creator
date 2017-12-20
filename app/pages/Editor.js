8a075fb05ebb9c5196210651717f202544e46924
Luciano Di Pasquale <yo@lucianodipasquale.me>
2017-12-20 14:27:46 -0300

    Use mock from endpoint

--- app/pages/Editor.js
+++ app/pages/Editor.js
@@ -87,7 +87,7 @@ class Editor extends React.Component {
   handleSubmitPalette() {
     const palette = {
       name: this.state.name,
-      modifiers: this.state.modifiers,
+      fields: this.state.fields,
       templates: this.state.templates.map(template => ({
         width: template.width,
         height: template.height,
